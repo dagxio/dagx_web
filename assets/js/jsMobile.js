bd981c0691db06e2fae047a6898ecac296ef9053
zero <wangyue@outman.com>
2018-07-06 17:05:12 +0800

    fix

---
+++ assets/js/jsMobile.js
@@ -0,0 +1,20 @@
+function isMobile() {
+    var regex_match = /(nokia|iphone|android|motorola|^mot-|softbank|foma|docomo|kddi|up.browser|up.link|htc|dopod|blazer|netfront|helio|hosin|huawei|novarra|CoolPad|webos|techfaith|palmsource|blackberry|alcatel|amoi|ktouch|nexian|samsung|^sam-|s[cg]h|^lge|ericsson|philips|sagem|wellcom|bunjalloo|maui|symbian|smartphone|midp|wap|phone|windows ce|iemobile|^spice|^bird|^zte-|longcos|pantech|gionee|^sie-|portalmmm|jigs browser|hiptop|^benq|haier|^lct|operas*mobi|opera*mini|320x320|240x320|176x220)/i;
+    var u = navigator.userAgent;
+    if (null == u) {
+        return true;
+    }
+    var result = regex_match.exec(u);
+
+    if (null == result) {
+        return false
+    } else {
+        return true
+    }
+}
+
+function autoJump(url){
+    if (isMobile() && localStorage.userSetDevice!='PC'){
+        window.location.href = url;
+    }
+}
